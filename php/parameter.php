b2db4c8cf3f5984751f728037616224e4a98c478
ykh4933 <ykh4933@naver.com>
2020-10-09 15:47:45 +0900

    parameter get방식

---
+++ php/parameter.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    안녕하세요.  <?php echo $_GET['address']; ?>에 사시는  <?php echo $_GET['name']; ?>님
+
+  </body>
+</html>
