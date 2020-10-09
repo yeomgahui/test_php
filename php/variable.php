b87949e818e964a4075e9e153a8f40fde175d10a
ykh4933 <ykh4933@naver.com>
2020-10-09 15:33:28 +0900

    php 변수

---
+++ php/variable.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Variable</h1>
+<?php
+  $a = 10;
+  echo $a+1;
+?>
+<?php
+$name = "gahui";
+echo "Lorem ipsum  ".$name." dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt".$name." ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation".$name." ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum ".$name." eu fugiat nulla pariatur.".$name." Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ".$name." deserunt mollit anim id est laborum.";
+?>
+
+</body>
+</html>
